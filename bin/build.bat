c3f92d50407b08364233ca82343e8d87dbb1a2bc
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2016-03-23 09:45:15 +0100

    fix unzip source

--- bin/build.bat
+++ bin/build.bat
@@ -1,4 +1,12 @@
 @echo off
+
+buildconf
+
+configure.bat --help | finstr amqp
+configure.bat --help | finstr redis
+configure.bat --help | finstr apcu
+configure.bat --help | finstr wincache
+
 rem  "--with-pgo"
 configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-apcu" "--enable-object-out-dir=../obj/" "--with-ldap" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer"
 
