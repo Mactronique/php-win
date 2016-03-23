7be639e17486177ea8eb247da309177f22862f76
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2016-03-23 13:31:03 +0100

    fix change dir

--- bin/build.bat
+++ bin/build.bat
@@ -2,8 +2,8 @@
 
 call buildconf
 
-call configure.bat --help | finstr amqp
-call configure.bat --help | finstr redis
+call configure.bat --help | findstr amqp
+call configure.bat --help | findstr redis
 
 rem  "--with-pgo"
 call configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-object-out-dir=../obj/" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer" >compile.log 2>compile-errors.log
