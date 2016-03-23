595a68115af35d53d5ab7042e8ea296480fa5954
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2016-03-23 13:36:58 +0100

    fix log addition

--- bin/build.bat
+++ bin/build.bat
@@ -8,6 +8,6 @@ call configure.bat --help | findstr redis
 rem  "--with-pgo"
 call configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-object-out-dir=../obj/" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer" >compile.log 2>compile-errors.log
 
-nmake >compile.log 2>compile-errors.log
+nmake >>compile.log 2>>compile-errors.log
 
-nmake build-dist >compile.log 2>compile-errors.log
+nmake build-dist >>compile.log 2>>compile-errors.log
