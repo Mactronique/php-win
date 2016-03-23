282acdcee5d925a5dad9f81a619d0f98fc108ea6
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2016-03-23 11:56:31 +0100

    change cp by copy

--- bin/build.bat
+++ bin/build.bat
@@ -6,8 +6,8 @@ configure.bat --help | finstr amqp
 configure.bat --help | finstr redis
 
 rem  "--with-pgo"
-configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-object-out-dir=../obj/" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer"
+configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-object-out-dir=../obj/" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer" >compile.log 2>compile-errors.log
 
-nmake
+nmake >compile.log 2>compile-errors.log
 
-nmake build-dist
+nmake build-dist >compile.log 2>compile-errors.log
