8819e899f2e0ccac27d41c0eb000e74b5a163805
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2016-03-23 13:22:07 +0100

    fix build script

--- bin/build.bat
+++ bin/build.bat
@@ -1,12 +1,12 @@
 @echo off
 
-buildconf
+call buildconf
 
-configure.bat --help | finstr amqp
-configure.bat --help | finstr redis
+call configure.bat --help | finstr amqp
+call configure.bat --help | finstr redis
 
 rem  "--with-pgo"
-configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-object-out-dir=../obj/" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer" >compile.log 2>compile-errors.log
+call configure.bat "--enable-snapshot-build" "--enable-debug-pack" "--disable-zts" "--with-amqp=shared" "--enable-object-out-dir=../obj/" "--enable-phar-native-ssl" "--with-sqlite3" "--with-pdo-sqlite" "--enable-odbc" "--with-pdo-odbc" "--enable-redis=shared" "--enable-com-dotnet=shared" "--with-mcrypt=static" "--without-analyzer" >compile.log 2>compile-errors.log
 
 nmake >compile.log 2>compile-errors.log
 
